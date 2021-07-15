37a9b839f23896cf0d13b25e6d05354df7ae6219
kwin64 <kwin11bvb@yandex.by>
2021-07-15 23:46:31 +0300

    request local server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+    const [state, setState] = useState<Array<any>>([])
+
+    useEffect(() => {
+        axios.get('http://localhost:7542/users')
+            .then(res =>
+                setState(res.data)
+            )
+    }, [])
+
     return (
         <div>
-            Store
+            {state.map(a => <div>{a.name}</div>)}
         </div>
     );
 }
