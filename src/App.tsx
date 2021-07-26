7322c1fce0fc130dafe1844423b813144ef34fe0
kwin64 <kwin11bvb@yandex.by>
2021-07-26 22:42:35 +0300

    test input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 function App() {
 
     const [users, setUsers] = useState<Array<any>>([])
+    const [newName, setNewName] = useState<string | null>(null)
 
     let getUser = () => {
         axios.get('http://localhost:7542/users')
@@ -17,11 +18,12 @@ function App() {
     }, [])
 
     let createUser = () => {
-        axios.post('http://localhost:7542/users')
+        axios.post('http://localhost:7542/users', {name: newName})
             .then(res =>
                 getUser()
             )
     }
+
     let deleteUser = () => {
         axios.delete('http://localhost:7542/users')
             .then(res =>
@@ -29,10 +31,15 @@ function App() {
             )
     }
 
-    console.log(users)
+    const newValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewName(e.currentTarget.value)
+    }
 
     return (
         <div>
+            <div>
+                <input type="text" onChange={newValueHandler}/>
+            </div>
             <button onClick={createUser}>user</button>
             <button onClick={deleteUser}>delete</button>
             {users.map(a => <div>{a.name}</div>)}
