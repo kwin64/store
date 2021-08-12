449bd66ac6d9d438c4ac508ef1c9e8f6b390e7ef
kwin64 <kwin11bvb@yandex.by>
2021-08-12 16:43:57 +0300

    slide changer

--- src/PageSlider/PageSlider.tsx
+++ src/PageSlider/PageSlider.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import './PageSlider.scss'
+import backArrow from '../common/backArrow.png'
+import nextArrow from '../common/nextArrow.png'
 
 export const PageSlider = () => {
+
+    const prevSlide = () => {
+
+    }
+    const nextSlide = () => {
+
+    }
+
     return (
-        <div>
-            PageSlider
+        <div className={'slider'}>
+            <div className={'slideChanger'}>
+                <a href="" onClick={prevSlide}>
+                    <img src={backArrow} alt=""/>
+                </a>
+                <a href="" onClick={nextSlide}>
+                    <img src={nextArrow} alt="" onClick={nextSlide}/>
+                </a>
+            </div>
+            <div>
+                <div>slide 1</div>
+                <div>slide 2</div>
+                <div>slide 3</div>
+            </div>
         </div>
     )
 }
