444b58635c5e848c0067c05cd2f78646c98b7fcd
kwin64 <kwin11bvb@yandex.by>
2021-08-13 16:16:17 +0300

    add swiper, create slider for PageSlider

--- src/PageSlider/PageSlider.tsx
+++ src/PageSlider/PageSlider.tsx
@@ -1,32 +1,47 @@
 import React from 'react';
 import './PageSlider.scss'
-import backArrow from '../common/backArrow.png'
-import nextArrow from '../common/nextArrow.png'
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import SwiperCore, {Pagination} from 'swiper/core';
+import js from '../common/slider/js.jpg'
+import html from '../common/slider/html.jpg'
+import react from '../common/slider/react.jpg'
+
+SwiperCore.use([Pagination]);
 
 export const PageSlider = () => {
-
-    const prevSlide = () => {
-
-    }
-    const nextSlide = () => {
-
-    }
-
     return (
+
         <div className={'slider'}>
-            <div className={'slideChanger'}>
-                <a href="" onClick={prevSlide}>
-                    <img src={backArrow} alt=""/>
-                </a>
-                <a href="" onClick={nextSlide}>
-                    <img src={nextArrow} alt="" onClick={nextSlide}/>
-                </a>
-            </div>
-            <div>
-                <div>slide 1</div>
-                <div>slide 2</div>
-                <div>slide 3</div>
-            </div>
+            <Swiper pagination={{"dynamicBullets": true}}>
+                <div className={'slides'}>
+                    <SwiperSlide>
+                        <div className={'slide'}>
+                            <img src={html} alt=""/>
+                            <div className={'linkContent'}>
+                                <a href="">Hello</a>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <div className={'slide'}>
+                        <SwiperSlide>
+                            <img src={js} alt=""/>
+                            <div className={'linkContent'}>
+                                <a href="">Hello</a>
+                            </div>
+                        </SwiperSlide>
+                    </div>
+                    <div className={'slide'}>
+                        <SwiperSlide>
+                            <img src={react} alt=""/>
+                            <div className={'linkContent'}>
+                                <a href="">Hello</a>
+                            </div>
+                        </SwiperSlide>
+                    </div>
+                </div>
+            </Swiper>
         </div>
     )
 }
