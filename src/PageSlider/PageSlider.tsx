11859467e06973967a1d80201a6e7c680db38e7a
kwin64 <kwin11bvb@yandex.by>
2021-08-13 17:22:18 +0300

    create visual slider page

--- src/PageSlider/PageSlider.tsx
+++ src/PageSlider/PageSlider.tsx
@@ -4,7 +4,7 @@ import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css"
 import SwiperCore, {Pagination} from 'swiper/core';
-import js from '../common/slider/js.jpg'
+import js from '../common/slider/js.png'
 import html from '../common/slider/html.jpg'
 import react from '../common/slider/react.jpg'
 
@@ -16,30 +16,34 @@ export const PageSlider = () => {
         <div className={'slider'}>
             <Swiper pagination={{"dynamicBullets": true}}>
                 <div className={'slides'}>
-                    <SwiperSlide>
-                        <div className={'slide'}>
+
+                    <div className={'slide'}>
+                        <SwiperSlide>
                             <img src={html} alt=""/>
                             <div className={'linkContent'}>
-                                <a href="">Hello</a>
+                                <a href="#">HTML/CSS</a>
                             </div>
-                        </div>
-                    </SwiperSlide>
+                        </SwiperSlide>
+                    </div>
+
                     <div className={'slide'}>
                         <SwiperSlide>
                             <img src={js} alt=""/>
                             <div className={'linkContent'}>
-                                <a href="">Hello</a>
+                                <a href="#">JavaScript</a>
                             </div>
                         </SwiperSlide>
                     </div>
+
                     <div className={'slide'}>
                         <SwiperSlide>
                             <img src={react} alt=""/>
                             <div className={'linkContent'}>
-                                <a href="">Hello</a>
+                                <a href="#">ReactJS</a>
                             </div>
                         </SwiperSlide>
                     </div>
+
                 </div>
             </Swiper>
         </div>
